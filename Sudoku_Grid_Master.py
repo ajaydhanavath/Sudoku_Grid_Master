29e8c14aea7f7a10247d90551a79cdb59872c333
ajaydhanavath <111749226+ajaydhanavath@users.noreply.github.com>
2023-08-16 11:21:11 +0530

    Add files via upload

---
+++ Sudoku_Grid_Master.py
@@ -0,0 +1,51 @@
+def is_valid(board, row, col, num):
+    for i in range(9):
+        if board[row][i] == num or board[i][col] == num:
+            return False
+    start_row, start_col = 3 * (row // 3), 3 * (col // 3)
+    for i in range(3):
+        for j in range(3):
+            if board[start_row + i][start_col + j] == num:
+                return False
+    return True
+
+def find_empty_location(board, empty_spot):
+    for row in range(9):
+        for col in range(9):
+            if board[row][col] == 0:
+                empty_spot[0], empty_spot[1] = row, col
+                return True
+    return False
+
+def solve_sudoku(board):
+    empty_spot = [0, 0]
+    if not find_empty_location(board, empty_spot):
+        return True
+    row, col = empty_spot
+
+    for num in range(1, 10):
+        if is_valid(board, row, col, num):
+            board[row][col] = num
+            if solve_sudoku(board):
+                return True
+            board[row][col] = 0
+    return False
+
+# Example usage
+sudoku_board = [
+    [5, 3, 0, 0, 7, 0, 0, 0, 0],
+    [6, 0, 0, 1, 9, 5, 0, 0, 0],
+    [0, 9, 8, 0, 0, 0, 0, 6, 0],
+    [8, 0, 0, 0, 6, 0, 0, 0, 3],
+    [4, 0, 0, 8, 0, 3, 0, 0, 1],
+    [7, 0, 0, 0, 2, 0, 0, 0, 6],
+    [0, 6, 0, 0, 0, 0, 2, 8, 0],
+    [0, 0, 0, 4, 1, 9, 0, 0, 5],
+    [0, 0, 0, 0, 8, 0, 0, 7, 9]
+]
+
+if solve_sudoku(sudoku_board):
+    for row in sudoku_board:
+        print(row)
+else:
+    print("No solution exists.")
